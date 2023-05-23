8ddd44c2b928813211eb2901ea1e730b5637bbd1
RABI <rabi@RABIs-MacBook-Air.local>
2023-05-24 01:04:03 +0530

    single commit

---
+++ Asycimage/AsycimageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsycimageApp.swift
+//  Asycimage
+//
+//  Created by RABI on 23/05/23.
+//
+
+import SwiftUI
+
+@main
+struct AsycimageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
